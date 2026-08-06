11d374f80f422e8f26b0bfecc55c2f8e436ff2ba
ct1150 <ct1150@gmail.com>
2026-08-06 23:39:00 +0800

    feat(trips): expose Traditional Chinese workspace [skip ci]

---
+++ apps/web/src/components/TraditionalTripWorkspace.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import type { ReactElement } from "react";
+import { LocalizedTripWorkspace } from "./LocalizedTripWorkspace";
+
+export function TraditionalTripWorkspace(): ReactElement {
+  return <LocalizedTripWorkspace locale="zh-hant" />;
+}
